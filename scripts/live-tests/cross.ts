37b50e066e983f9dee8d37a85577906811181b0b
lukmaan-ferrum <lukmaan.kolia@ferrum.network>
2024-09-03 15:42:39 +0100

    wip

--- scripts/live-tests/cross.ts
+++ scripts/live-tests/cross.ts
@@ -9,7 +9,8 @@ async function main() {
     const recipient = "0x93069da82B264E94068aA991b88b3478cf0861BE"
     const foundryAddress = "0xF45fCD48BdAB3156fF31a74161a454afa749a170"
     const qpFeeTokenAddress = "0x4acc6E4ae8b93d7E5474b0aBb0680D1A16b2f94f"
-    const amountIn = 10000000n
+    const amountIn = 8000000000000000000n
+    const qpFeeAmount = 10n ** 18n
 
     const mockFoundry = await hre.ethers.getContractAt("Token", foundryAddress);
     const qpFeeToken = await hre.ethers.getContractAt("Token", qpFeeTokenAddress);
@@ -18,24 +19,25 @@ async function main() {
     approveFoundryTx.wait()
     console.log("Approved Foundry")
 
-    const approveQpFeeTokenTx = await qpFeeToken.approve(fiberRouterAddress, 10n ** 18n)
+    const approveQpFeeTokenTx = await qpFeeToken.approve(fiberRouterAddress, qpFeeAmount)
     approveQpFeeTokenTx.wait()
     console.log("Approved QP Fee Token")
 
     const tx = await fiberRouter.cross(
         mockFoundry,
         amountIn,
-        10n ** 18n,
+        qpFeeAmount,
         recipient,
-        56,
+        42161,
         0,
-        "0x"
+        "0x",
+        {
+            gasLimit: 5000000
+        }
     )
 
     console.log("hash: ", tx.hash)
-
     await tx.wait()
-
     console.log("confirmed")
 }
 
